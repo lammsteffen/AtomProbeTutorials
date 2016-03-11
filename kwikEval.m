92dd8487633527f562a301eccbebd8d4c6ed2c25
peterfelfer <peter.felfer@fau.de>
2016-03-11 08:11:12 +0100

    W11/16

--- kwikEval.m
+++ kwikEval.m
@@ -1,4 +1,4 @@
-function kwikEval(pos,parentPos,interface)
+function kwikEval(pos,parentPos,interface,vertices)
 %calculates the average interfacial excess for an interface with an
 %interactive interface for the IE determination
 
@@ -15,19 +15,34 @@ addpath('resources');
 
 normals = patchnormals(interface);
 
+numVerts = length(interface.vertices);
 %% tessellation and distance clipping
 % finding closest point for each atomic position
 closest = dsearchn(interface.vertices,delaunayn(interface.vertices),parentPos(:,1:3));
+
+% calculation of vertex area
+[cp,ce,pv,ev] = makedual2(interface.vertices,interface.faces);
+[pc,area] = geomdual2(cp,ce,pv,ev);
+
+
+%if local IE values are calcualted, only atomic positions associated with
+%the vertices on the vertex list are used.
+if exist('vertices','var')
+    isLocal = ismember(closest,vertices);
+    parentPos = parentPos(isLocal,:);
+    closest = closest(isLocal,:);
+    
+    area = area(vertices);
+end
+
+
 distVec = parentPos(:,1:3) - interface.vertices(closest,:);
 %distance through dot product
 dist = sum(normals(closest,:) .* distVec,2);
-
-
-
-%% calculation of vertex area
-[cp,ce,pv,ev] = makedual2(interface.vertices,interface.faces);
-[pc,area] = geomdual2(cp,ce,pv,ev);
 area = sum(area);
+
+
+
 
 
 
